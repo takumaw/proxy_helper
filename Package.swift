2bd41efc37f17cc7814be3d03d9373eb0818a17c
Takuma Watanabe <takumaw@sfo.kuramae.ne.jp>
2018-12-28 21:05:56 +0900

    Removed SwiftPM dependencies to reduce binary size.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ProxyHelper",
-            dependencies: ["Utility"]),
+            dependencies: []),
         .testTarget(
             name: "ProxyHelperTests",
             dependencies: ["ProxyHelper"]),
--- Sources/ProxyHelper/main.swift
+++ Sources/ProxyHelper/main.swift
@@ -4,9 +4,6 @@
 
 import Foundation
 
-import Basic
-import Utility
-
 /**
  ProxyHelper entry point class.
  */
@@ -25,26 +22,10 @@ class Main {
      */
     public func main(_ arguments: [String]) -> Int32 {
         
-        let parser = ArgumentParser(commandName: "proxy_helper",
-                                    usage: "[-c | -s]",
-                                    overview: "Helper for constructing proxy environment variable",
-                                    seeAlso: nil)
+        var shellStyle: ShellStyle = .BourneShell
         
-        let cShellEnabledOption = parser.add(option: "-c", kind: Bool.self,
-                                             usage: "Generate C-shell commands on stdout.  This is the default if SHELL ends with \"csh\".")
-        let bourneShellEnabledOption = parser.add(option: "-s", kind: Bool.self,
-                                                  usage: "Generate Bourne shell commands on stdout.  This is the default if SHELL does not end with \"csh\".")
-        
-        do {
-            let parsedResult: ArgumentParser.Result = try parser.parse(Array(arguments.dropFirst()))
-            
-            var shellStyle: ShellStyle = .BourneShell
-            
-            if let _: Bool = parsedResult.get(bourneShellEnabledOption) {
-                shellStyle = .BourneShell
-            } else if let _: Bool = parsedResult.get(cShellEnabledOption) {
-                shellStyle = .CShell
-            } else if let shellEnv: String = ProcessInfo.processInfo.environment["SHELL"] {
+        if (arguments.count == 1) {
+            if let shellEnv: String = ProcessInfo.processInfo.environment["SHELL"] {
                 if shellEnv.hasSuffix("csh") {
                     shellStyle = .CShell
                 } else {
@@ -53,20 +34,19 @@ class Main {
             } else {
                 shellStyle = .BourneShell
             }
-            
-            Main.proxyHelper.printProxySettings(shellStyle: shellStyle)
-            
-            return 0
-        } catch let error as ArgumentParserError {
-            Main.console.err(error.description)
-            parser.printUsage(on: stderrStream)
-            
-            return EINVAL
-        } catch let error {
-            Main.console.err(error.localizedDescription)
-            
-            return EINVAL
+        } else {
+            if (arguments[1] == "-c") {
+                shellStyle = .CShell
+            } else if (arguments[1] == "-s") {
+                shellStyle = .BourneShell
+            } else {
+                shellStyle = .BourneShell
+            }
         }
+        
+        Main.proxyHelper.printProxySettings(shellStyle: shellStyle)
+        
+        return 0
     }
     
 }
