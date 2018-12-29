55ab55b90d20ed4aa988b5b739811c540dec2e37
Takuma Watanabe <takumaw@sfo.kuramae.ne.jp>
2018-12-29 12:19:54 +0900

    Defined products section to specify output bianry name.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "ProxyHelper",
+    products: [
+        .executable(
+            name: "proxy_helper",
+            targets: ["ProxyHelper"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
