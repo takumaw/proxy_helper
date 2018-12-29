e3651fab7dbe82e41895321a8ba2b66b96c84edf
Takuma Watanabe <takumaw@sfo.kuramae.ne.jp>
2018-12-29 23:38:05 +0900

    Added default document comment.

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ProxyHelper",
     products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .executable(
             name: "proxy_helper",
             targets: ["ProxyHelper"]),
