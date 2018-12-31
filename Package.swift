85ee61a4bca378b17b007f1734aa06aa0f462ad8
Takuma Watanabe <takumaw@sfo.kuramae.ne.jp>
2018-12-31 20:22:44 +0900

    Channged package name.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "ProxyHelper",
+    name: "proxy_helper",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .executable(
