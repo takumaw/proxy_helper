f6ed36a8294d53ed36e301a4d2bd0d712afa1418
Takuma Watanabe <takumaw@sfo.kuramae.ne.jp>
2026-06-16 22:07:25 +0900

    test: add comprehensive test suite and rename proxy binary path in ProxyHelperTests

--- Tests/ProxyHelperTests/ProxyHelperTests.swift
+++ Tests/ProxyHelperTests/ProxyHelperTests.swift
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Takuma Watanabe.
+ * Copyright (c) 2018-2026 Takuma Watanabe.
  */
 
 import XCTest
@@ -8,16 +8,20 @@ import class Foundation.Bundle
 final class ProxyHelperTests: XCTestCase {
     
     static var allTests = [
-        ("quitsCorrectly", quitsCorrectly),
+        ("testQuitsCorrectly", testQuitsCorrectly),
+        ("testShellOptionBourne", testShellOptionBourne),
+        ("testShellOptionCShell", testShellOptionCShell),
+        ("testPACWithoutSettings", testPACWithoutSettings),
+        ("testInvalidArguments", testInvalidArguments),
     ]
     
-    func quitsCorrectly() throws {
+    func testQuitsCorrectly() throws {
         // Some of the APIs that we use below are available in macOS 10.13 and above.
         guard #available(macOS 10.13, *) else {
             return
         }
 
-        let fooBinary = productsDirectory.appendingPathComponent("ProxyHelper")
+        let fooBinary = productsDirectory.appendingPathComponent("proxy_helper")
 
         let process = Process()
         process.executableURL = fooBinary
@@ -31,11 +35,102 @@ final class ProxyHelperTests: XCTestCase {
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
         let output = String(data: data, encoding: .utf8)
         
-        // TODO: Write it later.
-        //XCTAssertEqual(output, "...")
         print(output as Any)
         
         XCTAssertEqual(process.terminationStatus, 0)
+    }
+
+    func testShellOptionBourne() throws {
+        guard #available(macOS 10.13, *) else {
+            return
+        }
+
+        let fooBinary = productsDirectory.appendingPathComponent("proxy_helper")
+
+        let process = Process()
+        process.executableURL = fooBinary
+        process.arguments = ["-s"]
+
+        let pipe = Pipe()
+        process.standardOutput = pipe
+
+        try process.run()
+        process.waitUntilExit()
+
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: .utf8) ?? ""
+        
+        if !output.isEmpty {
+            XCTAssertTrue(output.contains("export"))
+            XCTAssertFalse(output.contains("setenv"))
+        }
+        
+        XCTAssertEqual(process.terminationStatus, 0)
+    }
+
+    func testShellOptionCShell() throws {
+        guard #available(macOS 10.13, *) else {
+            return
+        }
+
+        let fooBinary = productsDirectory.appendingPathComponent("proxy_helper")
+
+        let process = Process()
+        process.executableURL = fooBinary
+        process.arguments = ["-c"]
+
+        let pipe = Pipe()
+        process.standardOutput = pipe
+
+        try process.run()
+        process.waitUntilExit()
+
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: .utf8) ?? ""
+        
+        if !output.isEmpty {
+            XCTAssertTrue(output.contains("setenv"))
+            XCTAssertFalse(output.contains("export"))
+        }
+        
+        XCTAssertEqual(process.terminationStatus, 0)
+    }
+
+    func testPACWithoutSettings() throws {
+        guard #available(macOS 10.13, *) else {
+            return
+        }
+
+        let fooBinary = productsDirectory.appendingPathComponent("proxy_helper")
+
+        let process = Process()
+        process.executableURL = fooBinary
+        process.arguments = ["-p"]
+
+        let pipe = Pipe()
+        process.standardOutput = pipe
+
+        try process.run()
+        process.waitUntilExit()
+
+        XCTAssertEqual(process.terminationStatus, 0)
+    }
+
+    func testInvalidArguments() throws {
+        guard #available(macOS 10.13, *) else {
+            return
+        }
+
+        let fooBinary = productsDirectory.appendingPathComponent("proxy_helper")
+
+        let process = Process()
+        process.executableURL = fooBinary
+        process.arguments = ["-p", "-u"]
+
+        try process.run()
+        process.waitUntilExit()
+
+        XCTAssertEqual(process.terminationStatus, 1)
     }
 
     /// Returns path to the built products directory.
@@ -50,3 +145,4 @@ final class ProxyHelperTests: XCTestCase {
       #endif
     }
 }
+
